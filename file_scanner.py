12a383fd855bc29f1933609fa68c098e0f66d50a
Matus Dubrava <matusdubrava@gmail.com>
2024-03-07 22:11:41 +0100

    refactor into Scan class

--- file_scanner.py
+++ file_scanner.py
@@ -14,36 +14,18 @@ import apsw
 import pandas as pd
 from pydantic import BaseModel, Field
 
-CONFIG_PATH = "/home/matus/blob/config.json"
-REPORTS_DIR = "/home/matus/blob/reports"
-LOG_PATH = "/home/matus/blob/scan.log"
-LOG_TO_CONSOLE = False  # Enable for easier debugging
-
-# ====================================== LOGGING ====================================================
-logger = logging.getLogger("scan_logger")
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-console_handler.setFormatter(formatter)
-
-file_handler = RotatingFileHandler(LOG_PATH, maxBytes=5 * 1024 * 1024, backupCount=2)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-
-if LOG_TO_CONSOLE:
-    logger.addHandler(console_handler)
-# ===================================================================================================
+print(os.path.abspath(__file__))
+CONFIG_PATH = "/home/matus/apps/file_scanner/config.json"
+LOG_TO_CONSOLE = True  # Enable for easier debugging
 
 
 class ScanConfig(BaseModel):
     scan_paths: List[str]
     database_filepath: str
     scan_period_wait_time_hours: int
+    csv_dump_path: str
+    log_file: str
+    reports_path: str
 
 
 class TableDescription(BaseModel):
@@ -70,41 +52,214 @@ class TableDescription(BaseModel):
             return ""
 
 
-TRACKING_TABLES = {
-    "file": TableDescription(
-        table_name="file",
-        file_path="/home/matus/blob/file_records.dat",
-        lock=threading.Lock(),
-        columns=[
-            ("date__inode", "TEXT"),  # PK shortcut - date + inode
-            ("date_scanned", "DATETIME"),
-            ("inode", "INTEGER"),
-            ("filename", "TEXT"),
-            ("filepath", "TEXT"),
-            ("filetype", "TEXT"),
-            ("lines", "INTEGER"),
-            ("date_created", "DATETIME"),
-            ("date_modified", "DATETIME"),
-            ("error_occured", "INTEGER"),
-            ("error_traceback", "TEXT"),
-        ],
-        primary_key=["date__inode"],
-        csv_dump_file="/home/matus/blob/files.csv",
-    ),
-    "scan": TableDescription(
-        table_name="scan",
-        file_path="/home/matus/blob/file_records.dat",
-        lock=threading.Lock(),
-        columns=[
-            ("date_scanned", "DATETIME"),
-            ("scan_time", "REAL"),
-            ("files_scanned", "INTEGER"),
-            ("files_skipped", "INTEGER"),
-        ],
-        primary_key=[],
-        csv_dump_file="/home/matus/blob/scans.csv",
-    ),
-}
+class Scan:
+    def __init__(self, scan_config: ScanConfig) -> None:
+        self.scan_config = scan_config
+
+        if not os.path.exists(self.scan_config.csv_dump_path):
+            os.makedirs(self.scan_config.csv_dump_path)
+
+        if not os.path.exists(os.path.dirname(self.scan_config.log_file)):
+            os.makedirs(os.path.dirname(self.scan_config.log_file))
+
+        if not os.path.exists(self.scan_config.reports_path):
+            os.makedirs(self.scan_config.reports_path)
+
+        if not os.path.exists(os.path.dirname(self.scan_config.database_filepath)):
+            os.makedirs(os.path.dirname(self.scan_config.database_filepath))
+
+        self.tracking_tables = {
+            "file": TableDescription(
+                table_name="file",
+                file_path=self.scan_config.database_filepath,
+                lock=threading.Lock(),
+                columns=[
+                    ("date__inode", "TEXT"),  # PK shortcut - date + inode
+                    ("date_scanned", "DATETIME"),
+                    ("inode", "INTEGER"),
+                    ("filename", "TEXT"),
+                    ("filepath", "TEXT"),
+                    ("filetype", "TEXT"),
+                    ("lines", "INTEGER"),
+                    ("date_created", "DATETIME"),
+                    ("date_modified", "DATETIME"),
+                    ("error_occured", "INTEGER"),
+                    ("error_traceback", "TEXT"),
+                ],
+                primary_key=["date__inode"],
+                csv_dump_file=os.path.join(self.scan_config.csv_dump_path, "files.csv"),
+            ),
+            "scan": TableDescription(
+                table_name="scan",
+                file_path=self.scan_config.database_filepath,
+                lock=threading.Lock(),
+                columns=[
+                    ("date_scanned", "DATETIME"),
+                    ("scan_time", "REAL"),
+                    ("files_scanned", "INTEGER"),
+                    ("files_skipped", "INTEGER"),
+                ],
+                primary_key=[],
+                csv_dump_file=os.path.join(self.scan_config.csv_dump_path, "scans.csv"),
+            ),
+        }
+
+        self.initialize_logger()
+
+    def initialize_logger(self) -> None:
+        self.logger = logging.getLogger("scan_logger")
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(formatter)
+
+        file_handler = RotatingFileHandler(
+            self.scan_config.log_file, maxBytes=5 * 1024 * 1024, backupCount=2
+        )
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+
+        self.logger.addHandler(file_handler)
+
+        if LOG_TO_CONSOLE:
+            self.logger.addHandler(console_handler)
+
+    def intialize_tracking_tables(self) -> None:
+        conn, curs = get_sqlite_conn(filepath=self.scan_config.database_filepath)
+
+        for table in self.tracking_tables.values():
+            sql = f"""
+                CREATE TABLE IF NOT EXISTS {table.table_name} (
+                    {table.columns_string}
+                    {table.primary_key_string}
+                )
+            """
+            curs.execute(sql)
+
+        conn.close()
+        curs.close()
+
+    def dump_db_to_csv(self):
+        for table in self.tracking_tables.values():
+            conn, curs = get_sqlite_conn(filepath=self.scan_config.database_filepath)
+            sql = f"""
+                SELECT * FROM {table.table_name}
+            """
+            curs.execute(sql)
+            try:
+                df = pandas_read_sql_without_warnings(sql, conn)
+                df.to_csv(table.csv_dump_file, index=False, header=True)
+            except apsw.ExecutionCompleteError:
+                # Apparently, this exception will be thrown when pandas
+                # is tring to get column description (curs.description) when
+                # no recrods exist in the queried table. Therefore we are skipping
+                # this error.
+                self.logger.debug("Skipping write to csv. No records found.")
+
+            curs.close()
+            conn.close()
+
+    def create_daily_file_report(self):
+        daily_report_total = os.path.join(
+            self.scan_config.reports_path, "daily_total.csv"
+        )
+        daily_report_by_type = os.path.join(
+            self.scan_config.reports_path, "daily_by_type.csv"
+        )
+
+        if os.path.exists(self.scan_config.reports_path):
+            shutil.rmtree(self.scan_config.reports_path)
+
+        os.makedirs(self.scan_config.reports_path)
+
+        conn, curs = get_sqlite_conn(self.tracking_tables["file"].file_path)
+
+        sql = f"""
+            SELECT
+                DATE(date_modified) as date,
+                filetype,
+                SUM(lines) as lines,
+                COUNT(*) as count_files
+            FROM {self.tracking_tables["file"].table_name} as t1
+            JOIN (                                              -- use this join to get only the latest record for
+                                                                -- each file to avoid counting duplicates
+                SELECT 
+                    inode,
+                    MAX(date_scanned) as max_date_scanned
+                FROM {self.tracking_tables["file"].table_name}
+                GROUP BY
+                    inode
+            ) as t2
+            ON t1.inode = t2.inode AND t1.date_scanned = t2.max_date_scanned
+            GROUP BY 
+                DATE(date_modified),
+                filetype
+            ORDER BY 
+                DATE(date_modified)
+        """
+
+        df = pandas_read_sql_without_warnings(sql, conn)
+        df.to_csv(daily_report_by_type, index=False, header=True)
+
+        sql = f"""
+            SELECT
+                DATE(date_modified) as date,
+                SUM(lines) as lines,
+                COUNT(*) as count_files
+            FROM {self.tracking_tables["file"].table_name} as t1
+            JOIN (                                              -- use this join to get only the latest record for
+                                                                -- each file to avoid counting duplicates
+                SELECT 
+                    inode,
+                    MAX(date_scanned) as max_date_scanned 
+                FROM {self.tracking_tables["file"].table_name}
+                GROUP BY 
+                    inode
+            ) as t2 
+            ON t1.inode = t2.inode AND t1.date_scanned = t2.max_date_scanned
+            GROUP BY
+                DATE(date_modified)
+        """
+
+        df = pandas_read_sql_without_warnings(sql, conn)
+        df.to_csv(daily_report_total, index=False, header=True)
+
+        curs.close()
+        conn.close()
+
+    def should_perform_scan(self) -> bool:
+        conn, curs = get_sqlite_conn(self.tracking_tables["scan"].file_path)
+        sql = f"""
+            SELECT MAX(date_scanned) FROM {self.tracking_tables["scan"].table_name}
+        """
+        curs.execute(sql)
+        data = curs.fetchall()
+        perform_scan = False
+
+        # "data[0][0] is None" condition is here because even if there are
+        # no row in the table, running "MAX(date_scanned)" will return one row
+        # where the value is set to None
+        if not len(data) or data[0][0] is None:
+            self.logger.debug("no previous scan detected")
+            perform_scan = True
+        elif datetime.now() - datetime_from_sqlite_datetime(data[0][0]) >= timedelta(
+            hours=scan_config.scan_period_wait_time_hours
+        ):
+            self.logger.debug(
+                f"time since last scan: {datetime.now() - datetime_from_sqlite_datetime(data[0][0])}"
+            )
+            perform_scan = True
+        else:
+            self.logger.debug(
+                f"time since last scan: {datetime.now() - datetime_from_sqlite_datetime(data[0][0])}"
+            )
+
+        curs.close()
+        conn.close()
+        return perform_scan
 
 
 def get_sqlite_conn(filepath: str) -> Tuple[apsw.Connection, Any]:
@@ -154,27 +309,6 @@ def pandas_read_sql_without_warnings(sql: str, conn: apsw.Connection) -> pd.Data
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=UserWarning)
         return pd.read_sql(sql, conn)
-
-
-def dump_db_to_csv(scan_config: ScanConfig):
-    for table in TRACKING_TABLES.values():
-        conn, curs = get_sqlite_conn(filepath=scan_config.database_filepath)
-        sql = f"""
-            SELECT * FROM {table.table_name}
-        """
-        curs.execute(sql)
-        try:
-            df = pandas_read_sql_without_warnings(sql, conn)
-            df.to_csv(table.csv_dump_file, index=False, header=True)
-        except apsw.ExecutionCompleteError:
-            # Apparently, this exception will be thrown when pandas
-            # is tring to get column description (curs.description) when
-            # no recrods exist in the queried table. Therefore we are skipping
-            # this error.
-            logger.debug("Skipping write to csv. No records found.")
-
-        curs.close()
-        conn.close()
 
 
 class LineCountStat(BaseModel):
@@ -255,22 +389,6 @@ def is_tracked(filepath: str) -> bool:
     return result.stdout.strip() == "true"
 
 
-def create_tracking_tables(scan_config: ScanConfig) -> None:
-    conn, curs = get_sqlite_conn(filepath=scan_config.database_filepath)
-
-    for table in TRACKING_TABLES.values():
-        sql = f"""
-            CREATE TABLE IF NOT EXISTS {table.table_name} (
-                {table.columns_string}
-                {table.primary_key_string}
-            )
-        """
-        curs.execute(sql)
-
-    conn.close()
-    curs.close()
-
-
 def insert_data(table: TableDescription, records: List[Any]) -> None:
     sql = f"""
         INSERT INTO {table.table_name} VALUES ({table.columns_placeholder_string})
@@ -280,71 +398,6 @@ def insert_data(table: TableDescription, records: List[Any]) -> None:
         curs.execute("begin")
         curs.executemany(sql, [record.to_tuple() for record in records])
         curs.execute("commit")
-
-
-def create_daily_file_report():
-    daily_report_total = os.path.join(REPORTS_DIR, "daily_total.csv")
-    daily_report_by_type = os.path.join(REPORTS_DIR, "daily_by_type.csv")
-
-    if os.path.exists(REPORTS_DIR):
-        shutil.rmtree(REPORTS_DIR)
-
-    os.makedirs(REPORTS_DIR)
-
-    conn, curs = get_sqlite_conn(TRACKING_TABLES["file"].file_path)
-
-    sql = f"""
-        SELECT
-            DATE(date_modified) as date,
-            filetype,
-            SUM(lines) as lines,
-            COUNT(*) as count_files
-        FROM {TRACKING_TABLES["file"].table_name} as t1
-        JOIN (                                              -- use this join to get only the latest record for
-                                                            -- each file to avoid counting duplicates
-            SELECT 
-                inode,
-                MAX(date_scanned) as max_date_scanned
-            FROM {TRACKING_TABLES["file"].table_name}
-            GROUP BY
-                inode
-        ) as t2
-        ON t1.inode = t2.inode AND t1.date_scanned = t2.max_date_scanned
-        GROUP BY 
-            DATE(date_modified),
-            filetype
-        ORDER BY 
-            DATE(date_modified)
-    """
-
-    df = pandas_read_sql_without_warnings(sql, conn)
-    df.to_csv(daily_report_by_type, index=False, header=True)
-
-    sql = f"""
-        SELECT
-            DATE(date_modified) as date,
-            SUM(lines) as lines,
-            COUNT(*) as count_files
-        FROM {TRACKING_TABLES["file"].table_name} as t1
-        JOIN (                                              -- use this join to get only the latest record for
-                                                            -- each file to avoid counting duplicates
-            SELECT 
-                inode,
-                MAX(date_scanned) as max_date_scanned 
-            FROM {TRACKING_TABLES["file"].table_name}
-            GROUP BY 
-                inode
-        ) as t2 
-        ON t1.inode = t2.inode AND t1.date_scanned = t2.max_date_scanned
-        GROUP BY
-            DATE(date_modified)
-    """
-
-    df = pandas_read_sql_without_warnings(sql, conn)
-    df.to_csv(daily_report_total, index=False, header=True)
-
-    curs.close()
-    conn.close()
 
 
 def collect_file_stats(filepath: str, scan_start_time: datetime) -> FileStat:
@@ -384,53 +437,23 @@ def collect_file_stats(filepath: str, scan_start_time: datetime) -> FileStat:
     )
 
 
-def should_perform_scan(scan_config: ScanConfig) -> bool:
-    conn, curs = get_sqlite_conn(TRACKING_TABLES["scan"].file_path)
-    sql = f"""
-        SELECT MAX(date_scanned) FROM {TRACKING_TABLES["scan"].table_name}
-    """
-    curs.execute(sql)
-    data = curs.fetchall()
-    perform_scan = False
-
-    # "data[0][0] is None" condition is here because even if there are
-    # no row in the table, running "MAX(date_scanned)" will return one row
-    # where the value is set to None
-    if not len(data) or data[0][0] is None:
-        logger.debug("no previous scan detected")
-        perform_scan = True
-    elif datetime.now() - datetime_from_sqlite_datetime(data[0][0]) >= timedelta(
-        hours=scan_config.scan_period_wait_time_hours
-    ):
-        logger.debug(
-            f"time since last scan: {datetime.now() - datetime_from_sqlite_datetime(data[0][0])}"
-        )
-        perform_scan = True
-    else:
-        logger.debug(
-            f"time since last scan: {datetime.now() - datetime_from_sqlite_datetime(data[0][0])}"
-        )
-
-    curs.close()
-    conn.close()
-    return perform_scan
-
-
 if __name__ == "__main__":
+    with open(CONFIG_PATH) as f:
+        scan_config = ScanConfig.model_validate_json(f.read())
+
+    scan = Scan(scan_config)
+
     try:
-        with open(CONFIG_PATH) as f:
-            scan_config = ScanConfig.model_validate_json(f.read())
+        scan.intialize_tracking_tables()
 
-        create_tracking_tables(scan_config)
-
-        if should_perform_scan(scan_config):
+        if scan.should_perform_scan():
             start_time = time.time()
             files_scanned = 0
             files_skipped = 0
             current_datetime = datetime.now()
 
             for filepath in scan_config.scan_paths:
-                logger.debug(f"scanning, root dir: {filepath}")
+                scan.logger.debug(f"scanning, root dir: {filepath}")
                 file_stats: List[FileStat] = []
 
                 for foldername, subfolders, filenames in os.walk(filepath):
@@ -445,11 +468,11 @@ if __name__ == "__main__":
                         else:
                             files_skipped += 1
 
-                insert_data(TRACKING_TABLES["file"], file_stats)
+                insert_data(scan.tracking_tables["file"], file_stats)
 
             scan_time = round(time.time() - start_time, 2)
             insert_data(
-                TRACKING_TABLES["scan"],
+                scan.tracking_tables["scan"],
                 [
                     ScanStat(
                         date_scanned=get_sqlite_datetime(current_datetime),
@@ -461,9 +484,9 @@ if __name__ == "__main__":
             )
 
         else:
-            logger.debug("skipping scan")
+            scan.logger.debug("skipping scan")
 
-        dump_db_to_csv(scan_config)
-        create_daily_file_report()
+        scan.dump_db_to_csv()
+        scan.create_daily_file_report()
     except Exception:
-        logger.error(str(traceback.format_exc()).replace("\n", ","))
+        scan.logger.error(str(traceback.format_exc()).replace("\n", ","))
