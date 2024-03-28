1f01668e24e45496e04a4033d54077b1fe8dd934
Matus Dubrava <matusdubrava@gmail.com>
2024-03-28 14:52:00 +0100

    test 'inti --recreate' error handling

--- metadata_manager/tests/test_init/test_recreate.py
+++ metadata_manager/tests/test_init/test_recreate.py
@@ -1,4 +1,5 @@
 import pytest
+import shutil
 import subprocess
 from subprocess import CalledProcessError
 
@@ -57,3 +58,45 @@ def test_recreatate_works_even_when_no_repository_exists(
     utils.assert_database_structure(
         db_path=working_dir.joinpath(mdm_config.md_dir_name, mdm_config.md_db_name)
     )
+
+
+@pytest.mark.c17979dad6
+@pytest.mark.init
+@pytest.mark.init_recreate
+@pytest.mark.cli
+@pytest.mark.sanity
+def test_errors_while_recreating_repos_are_handled_no_debug(
+    working_dir, mdm_config, init_cmd
+):
+    # Turning the repository's interal dir into a file will make 'init' fail as
+    # it expect directory.
+    md_path = working_dir.joinpath(mdm_config.md_dir_name)
+    md_path.touch()
+
+    proc = subprocess.run([*init_cmd, working_dir, "--recreate"], capture_output=True)
+    assert proc.returncode == 3
+    assert not proc.stdout
+    assert "fatal" in proc.stderr.decode().lower()
+    assert "traceback" not in proc.stderr.decode().lower()
+
+
+@pytest.mark.e0a60e251a
+@pytest.mark.init
+@pytest.mark.init_recreate
+@pytest.mark.cli
+@pytest.mark.sanity
+def test_errors_while_recreating_repos_are_handled_with_debug(
+    working_dir, mdm_config, init_cmd
+):
+    # Turning the repository's interal dir into a file will make 'init' fail as
+    # it expect directory.
+    md_path = working_dir.joinpath(mdm_config.md_dir_name)
+    md_path.touch()
+
+    proc = subprocess.run(
+        [*init_cmd, working_dir, "--recreate", "--debug"], capture_output=True
+    )
+    assert proc.returncode == 3
+    assert not proc.stdout
+    assert "fatal" in proc.stderr.decode().lower()
+    assert "traceback" in proc.stderr.decode().lower()
